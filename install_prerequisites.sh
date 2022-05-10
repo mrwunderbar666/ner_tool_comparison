dab271d21005a9f9bb33ae34f56b6d4418f42b46
Paul Balluff <paul.balluff@gmail.com>
2022-05-10 21:13:17 +0200

    updated requirements

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,1 +1,2 @@
+mkdir utils
 curl https://huggingface.co/datasets/conll2003/raw/main/conll2003.py -o utils/conll2003.py
--- r_packages.r
+++ r_packages.r
@@ -1,3 +1,8 @@
 pkg <- c("corpustools", "caret", "arrow", "stringi")
 
-install.packages(pkg)
+for (p in pkg) {
+  if (nchar(find.package(p)) == 0) {
+    install.packages(p)
+  }
+}
+
