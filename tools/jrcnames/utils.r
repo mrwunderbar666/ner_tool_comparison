23a03b11e846d4bb231284d73fdd3eb1c70265de
Paul Balluff <paul.balluff@univie.ac.at>
2022-05-16 17:23:48 +0200

    evaluate jrc names

--- tools/jrcnames/utils.r
+++ tools/jrcnames/utils.r
@@ -19,16 +19,22 @@ recode_results <- function(df) {
   
   codings <- as.data.frame(df)
   
-  codings$JRC_any_NE <- as.numeric(!is.na(codings$type))
-  codings[is.na(codings$type), ]$type <- ""
-  codings$JRC_NE <- "O"
+  if ("type" %in% colnames(codings)) {
   
-  filt_persons <- codings$type == 'P'
-  codings[filt_persons, 'JRC_NE'] <- "PER"
-  
-  filt_orgs <- codings$type == 'O'
-  codings[filt_orgs, 'JRC_NE'] <- "ORG"
-  
+    codings$JRC_any_NE <- as.numeric(!is.na(codings$type))
+    codings[is.na(codings$type), ]$type <- ""
+    codings$JRC_NE <- "O"
+    
+    filt_persons <- codings$type == 'P'
+    codings[filt_persons, 'JRC_NE'] <- "PER"
+    
+    filt_orgs <- codings$type == 'O'
+    codings[filt_orgs, 'JRC_NE'] <- "ORG"
+    
+  } else {
+    codings$JRC_any_NE <- 0
+    codings$JRC_NE <- "O"
+  }
   codings$JRC_NE <- factor(codings$JRC_NE, levels=levels(codings$NE))
   
   return(codings)  
@@ -36,15 +42,29 @@ recode_results <- function(df) {
 
 # Extract Relevant Information from caret::confusionMatrix
 cm2df <- function(confusion, corpus, lang) {
-  return(data.frame(
-    corpus = corpus, 
-    lang = lang, 
-    task = gsub("Class: ", "", names(confusion$byClass[, 'Precision'])), 
-    precision = unname(confusion$byClass[, 'Precision']),
-    recall = unname(confusion$byClass[, 'Recall']),
-    specificity = unname(confusion$byClass[, 'Specificity']),
-    f1 = unname(confusion$byClass[, 'F1'])
-  ))
+  if (is.null(confusion$positive)) {
+    return(data.frame(
+      corpus = corpus, 
+      lang = lang, 
+      task = gsub("Class: ", "", names(confusion$byClass[, 'Precision'])), 
+      precision = unname(confusion$byClass[, 'Precision']),
+      recall = unname(confusion$byClass[, 'Recall']),
+      specificity = unname(confusion$byClass[, 'Specificity']),
+      f1 = unname(confusion$byClass[, 'F1'])
+    ))
+    
+  }
+    else {
+      return(data.frame(
+        corpus = corpus, 
+        lang = lang, 
+        task = confusion$positive, 
+        precision = unname(confusion$byClass['Precision']),
+        recall = unname(confusion$byClass['Recall']),
+        specificity = unname(confusion$byClass['Specificity']),
+        f1 = unname(confusion$byClass['F1'])
+      ))
+  }
 }
 
 
--- tools/jrcnames/wikiann.r
+++ tools/jrcnames/wikiann.r
@@ -23,17 +23,19 @@ jrc_dict <- arrow::read_feather('tools/jrcnames/jrcnames.feather')
 # Pre-process: replace token separator '+' with ' '
 jrc_dict$keyword <- stringi::stri_replace_all(jrc_dict$keyword, fixed = '+', ' ')
 
-corpora <- Sys.glob('corpora/europeana/*.feather')
+
+corpora <- Sys.glob('corpora/wikiann/*_validation.feather')
 
 for (corpus in corpora) {
+  print(corpus)
   
   # Load corpus
   df <- arrow::read_feather(corpus)
   
-  df <- recode_iob(df, colname = 'CoNLL_IOB2')
+  df <- recode_iob(df, colname = 'IOB2')
   
   # Transform raw corpus to tcorpus obect
-  tc <- corpustools::tokens_to_tcorpus(df, doc_col = 'doc_id', token_id_col = 'token_id', token_col = 'token')
+  tc <- corpustools::tokens_to_tcorpus(df, doc_col = 'sentence', token_id_col = 'token_id', token_col = 'token')
   
   # Run Dictionary over Corpus
   tc$code_dictionary(jrc_dict, case_sensitive = T, token_col = 'token', string_col = 'keyword', sep = ' ', use_wildcards = F)
@@ -44,14 +46,14 @@ for (corpus in corpora) {
   # Calculate Evaluation Metrics
   cm <- caret::confusionMatrix(codings$JRC_NE, reference=codings$NE, mode = "everything")
   
-  r <- cm2df(cm, df$corpus[1], df$language[1])
+  r <- cm2df(cm, "wikiann", df$language[1])
   
   results <- rbind(results, r)
   
   cm <- caret::confusionMatrix(as.factor(codings$JRC_any_NE), reference=as.factor(codings$any_NE), mode = "everything")
   
   results <- rbind(results, data.frame(
-    corpus = df$corpus[1],
+    corpus = "wikiann",
     lang = df$language[1], 
     task = 'any_NE', 
     precision = cm$byClass['Precision'],
@@ -59,7 +61,7 @@ for (corpus in corpora) {
     specificity = cm$byClass['Specificity'],
     f1 = cm$byClass['F1']
   ))
-
+  
 }
 
 # Save results
@@ -68,4 +70,4 @@ if (!dir.exists('results')) {
   dir.create('results')
 }
 
-write.csv(results, file='results/jrc_europeana.csv', row.names = F)
+write.csv(results, file='results/jrc_wikiann.csv', row.names = F)
