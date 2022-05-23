e2ff047aabe8a42ba197a479dd52cd079f6676af
Paul Balluff <paul.balluff@univie.ac.at>
2022-05-23 18:18:15 +0200

    automatic evaluation of tools

---
+++ run_tools.sh
@@ -0,0 +1,14 @@
+# corenlp
+
+python3 tools/corenlp/get_corenlp.py
+python3 tools/corenlp/arabic.py
+python3 tools/corenlp/chinese.py
+python3 tools/corenlp/english.py
+python3 tools/corenlp/french.py
+python3 tools/corenlp/german.py
+python3 tools/corenlp/spanish.py
+
+# nltk
+
+python3 tools/nltk/get_dependencies.py
+python3 tools/nltk/english.py
