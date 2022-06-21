7cf62caf7a3502717217d0a7e2084f4a30b8af23
Paul Balluff <paul.balluff@univie.ac.at>
2022-06-21 09:19:15 +0200

    also add to registry

--- corpora/conll/conll2003_en_huggingface.py
+++ corpora/conll/conll2003_en_huggingface.py
@@ -1,7 +1,10 @@
 import sys
 from pathlib import Path
 sys.path.insert(0, str(Path.cwd()))
+# make sure to execute 'install_prerequisies.sh' before running this script
+# it automatically fetches the Huggingface file which processes the Conll2003 english dataset
 from utils.conll2003 import Conll2003
+from utils.registry import add_corpus
 
 p = Path.cwd() / 'corpora' / 'conll' 
 
@@ -26,18 +29,30 @@ for split in conll.keys():
     df.to_feather(p / f'conll2003_en_{split}_raw.feather', compression = 'uncompressed')
 
     df = df.explode(['tokens', 'pos_tags', 'chunk_tags', 'ner_tags'])
-    df['IOB2'] = df.ner_tags.replace(ner_tags)
+    df['CoNLL_IOB2'] = df.ner_tags.replace(ner_tags)
     df['sentence_id'] = df.id.astype(int)
+    df['sentence_id'] = df.sentence_id.astype(str).str.zfill(6)
     df['token_id'] = df.groupby('sentence_id').cumcount()
     df['token_id'] = df['token_id'] + 1
     df['token'] = df.tokens
     df['dataset'] = 'conll2003'
     df['language'] = 'en'
-    df['corpus'] = split
+    df['subset'] = split
 
-    df = df.loc[:, ['dataset', 'language', 'corpus', 'sentence_id', 'token_id', 'token', 'IOB2']]
+    df = df.loc[:, ['dataset', 'language', 'subset', 'sentence_id', 'token_id', 'token', 'CoNLL_IOB2']]
     df = df.loc[~df.token.isna(), ]
     df = df.reset_index(drop=True)
-    df.to_feather(p / f'conll2003_en_{split}_iob.feather', compression = 'uncompressed')
+    corpus_destination = p / f'conll2003_en_{split}_iob.feather'
+    df.to_feather(corpus_destination, compression = 'uncompressed')
+
+    corpus_details = {'corpus': 'conll', 
+                      'subset': split, 
+                      'path': corpus_destination, 
+                      'split': split,
+                      'language': 'en', 
+                      'tokens': len(df), 
+                      'sentences': len(df.sentence_id.unique())}
+    
+    add_corpus(corpus_details)
 
 
