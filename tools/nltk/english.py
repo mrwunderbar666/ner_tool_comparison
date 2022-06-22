5b2e4638013962e7baddf3a6aac83358dcff6bc9
Paul Balluff <paul.balluff@univie.ac.at>
2022-06-22 16:11:48 +0200

    now uses registry

--- tools/nltk/english.py
+++ tools/nltk/english.py
@@ -4,34 +4,35 @@ import pandas as pd
 from datasets import load_metric
 from timeit import default_timer as timer
 from datetime import timedelta
-
+from tqdm import tqdm
 import nltk
 
 sys.path.insert(0, str(Path.cwd()))
-from tools.nltk.utils import nltk2conll
+from utils.mappings import nltk2conll
+from utils.registry import load_registry
 
-language = 'english'
+language = 'en'
 p = Path.cwd() / 'tools' / 'nltk'
 results_path = Path.cwd() / 'results' / f'nltk_{language}.csv'
 
-corpora = {'conll': Path.cwd() / 'corpora' / 'conll' / 'conll2003_en_validation_iob.feather',
-           'emerging': Path.cwd() / 'corpora' / 'emerging' / 'emerging.test.annotated.feather',
-           'ontonotes': Path.cwd() / 'corpora' / 'ontonotes' / 'english_VALIDATION.feather',
-           'wikiann': Path.cwd() / 'corpora' / 'wikiann' / 'wikiann-en_validation.feather'
-           }
+registry = load_registry()
+
+corpora = registry.loc[(registry.language == language) & (registry.split == 'validation')]
 
 metric = load_metric("seqeval")
 evaluations = []
 
 print('Evaluating:', language)
 
-for corpus, path_corpus in corpora.items():
+for _, row in corpora.iterrows():
+
+    path_corpus = Path(row['path'])
 
     if not path_corpus.exists():
-        print('could not find corpus:', corpus)
+        print('could not find corpus:', path_corpus)
         continue
 
-    print('Loading corpus:', corpus)
+    print('Loading corpus:', path_corpus)
 
     df = pd.read_feather(path_corpus)
     df = df.loc[~df.token.isna(), ]
@@ -41,26 +42,27 @@ for corpus, path_corpus in corpora.items():
     df.sentence_id = df.sentence_id.astype(str).str.zfill(6)
 
     start_validation = timer()
-    print('Annotating...', corpus)
-
+    print('Annotating...', path_corpus)
         
     sentences = df.groupby('sentence_id')['token'].agg(list).tolist()
 
     classified = []
 
-    for sentence in sentences:
-        pos = nltk.pos_tag(sentence)
-        ner = nltk.ne_chunk(pos)
-        tagged = []
-        for token in ner:
-            if isinstance(token, tuple):
-                tagged.append('O')
-            else:
-                first = True
-                for t in token:
-                    tagged.append('B-' + token.label()) if first else tagged.append('I-' + token.label())
-                    first = False
-        classified.append(tagged)
+    with tqdm(total=len(sentences), unit='sentence') as pbar:
+        for sentence in sentences:
+            pos = nltk.pos_tag(sentence)
+            ner = nltk.ne_chunk(pos)
+            tagged = []
+            for token in ner:
+                if isinstance(token, tuple):
+                    tagged.append('O')
+                else:
+                    first = True
+                    for t in token:
+                        tagged.append('B-' + token.label()) if first else tagged.append('I-' + token.label())
+                        first = False
+            classified.append(tagged)
+            pbar.update(1)
 
     # rejoin with df
     df['nltk_ner'] = pd.Series(classified).explode().reset_index(drop=True)
@@ -70,11 +72,7 @@ for corpus, path_corpus in corpora.items():
     df['nltk_iob'] = df.nltk_ner.replace(nltk2conll)
 
     predictions = df.groupby('sentence_id')['nltk_iob'].agg(list).to_list()
-
-    if 'CoNLL_IOB2' in df.columns:
-        references = df.groupby('sentence_id')['CoNLL_IOB2'].agg(list).to_list()
-    else:
-        references = df.groupby('sentence_id')['IOB2'].agg(list).to_list()
+    references = df.groupby('sentence_id')['CoNLL_IOB2'].agg(list).to_list() 
 
     results = metric.compute(predictions=predictions, references=references)
 
@@ -87,10 +85,15 @@ for corpus, path_corpus in corpora.items():
 
     r = pd.DataFrame(r)
     r['language'] = language
-    r['corpus'] = corpus
+    r['corpus'] = row['corpus']
+    r['subset'] = row['subset']
     r['validation_duration'] = validation_time.total_seconds()
+    r['tokens'] = row['tokens']
+    r['sentences'] = row['sentences']
 
     evaluations.append(r)
 
 results_df = pd.concat(evaluations)
 results_df.to_csv(results_path, index=False)
+
+print('Done!')
