6f8d8048acf4cc034de3485c51cc60c27fa4ecb0
Paul Balluff <paul.balluff@univie.ac.at>
2022-10-12 18:20:19 +0200

    add packages for plotting

--- r_packages.r
+++ r_packages.r
@@ -1,4 +1,4 @@
-pkg <- c("corpustools", "caret", "arrow", "stringi", "udpipe", "nametagger", "dataverse")
+pkg <- c("corpustools", "caret", "arrow", "stringi", "udpipe", "nametagger", "dataverse", "showtext", "patchwork", "tidyverse", "RColorBrewer")
 
 for (p in pkg) {
     install.packages(p)
