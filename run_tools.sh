8df58420b4b998b08884e95b1b4ed376bbf42408
Paul Balluff <paul.balluff@gmail.com>
2022-10-17 11:28:10 +0200

    remove installation from evaluation

--- run_tools.sh
+++ run_tools.sh
@@ -1,17 +1,10 @@
 # corenlp
-python3 tools/corenlp/get_corenlp.py
 python3 tools/corenlp/evaluate.py
 
-# frog
-python3 tools/frog/install.py
-python3 tools/frog/evaluate.py
-
 # icews
-Rscript tools/icews/get_icews.r
 Rscript tools/icews/evaluate.r
 
 # jrcnames
-python3 tools/jrcnames/get_jrc.py
 Rscript tools/jrcnames/evaluate.r
 
 # nametagger
@@ -20,11 +13,9 @@ Rscript tools/nametagger/czech.r
 python3 tools/nametagger/evaluate.py
 
 # nltk
-python3 tools/nltk/get_dependencies.py
 python3 tools/nltk/english.py
 
 # opennlp
-python3 tools/opennlp/get_opennlp.py
 python3 tools/opennlp/evaluate.py
 
 # spacy
